71c06817a761e3ea7df49925727d5c3ee942398b
Nriver <6752679+Nriver@users.noreply.github.com>
2026-01-14 14:07:05 +0800

    update comments

--- lib/fullScreenWebPage.dart
+++ lib/fullScreenWebPage.dart
@@ -1,4 +1,19 @@
-// lib/pages/in_app_webview_fullscreen_page.dart
+// This file is part of pocket-trilium.
+
+// Copyright (C) 2026 Nriver
+
+// Pocket Trilium is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or any later version.
+
+// Pocket Trilium is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
-// main.dart  --  This file is part of tiny_computer.               
-                                                                        
-// Copyright (C) 2023 Caten Hu                                          
-                                                                        
-// Tiny Computer is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published    
-// by the Free Software Foundation, either version 3 of the License,    
-// or any later version.                               
-                                                                         
-// Tiny Computer is distributed in the hope that it will be useful,          
-// but WITHOUT ANY WARRANTY; without even the implied warranty          
-// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              
-// See the GNU General Public License for more details.                 
-                                                                     
-// You should have received a copy of the GNU General Public License    
-// along with this program.  If not, see http://www.gnu.org/licenses/.
+// This file is part of pocket-trilium.
+
+// Copyright (C) 2026 Nriver
+
+// Pocket Trilium is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or any later version.
+
+// Pocket Trilium is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html.
 
 
 import 'dart:async';
