219ddd096154ee1aee414c91d49b92494a7be41e
Nriver <6752679+Nriver@users.noreply.github.com>
2026-01-20 17:52:28 +0800

    remove FreeReflection

--- android/app/src/main/kotlin/com/example/tiny_computer/MainApplication.kt
+++ android/app/src/main/kotlin/com/example/tiny_computer/MainApplication.kt
@@ -3,7 +3,6 @@ package nriver.pocket.trilium
 import android.content.Context
 import com.google.android.material.color.DynamicColors
 import io.flutter.app.FlutterApplication
-import me.weishu.reflection.Reflection
 
 class MainApplication : FlutterApplication() {
 
@@ -14,6 +13,5 @@ class MainApplication : FlutterApplication() {
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        Reflection.unseal(base)
     }
 }
